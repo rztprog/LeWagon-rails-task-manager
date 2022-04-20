ddd698678d7b6429eed0ce8e9f48dc3c39fdda36
Rzt Prog <rztprog@outlook.fr>
2022-04-20 17:11:02 +0200

    Create seed and add faker gem

--- Gemfile
+++ Gemfile
@@ -52,3 +52,6 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 # SimpleForm
 gem 'simple_form'
+
+# Faker
+gem 'faker'
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-require 'faker'
+require "faker"
+
+# Run rails db:setup
 
 puts "
 ▀███▀▀▀██▄  ███▀▀▀█████▀▀██▀▀██████▀▀▀██▄▀███▀▀▀██▄   ▄▄█▀▀██▄   ▄▄█▀▀▀█▄█
@@ -42,39 +44,18 @@ puts "=============================="
 
 # Create users
 puts "Creating some task..."
+puts "How much tasks you want ?"
+puts "> "
+loop_n = STDIN.gets.chomp
 
-task = User.new(email: "nathan@lewagon.fr", password: "111111", first_name: "Nathan", last_name: "Jolivet")
-nathan.save
-
-file = URI.open('https://avatars.githubusercontent.com/u/45171753?v=4')
-luc = User.new(email: "luc@lewagon.fr", password: "222222", first_name: "Luc", last_name: "Broion")
-luc.photo.attach(io: file, filename: 'avatar', content_type: 'image')
-luc.save
-
-file = URI.open('https://avatars.githubusercontent.com/u/85166333?v=4')
-christophe = User.new(email: "christophe@lewagon.fr", password: "333333", first_name: "Christophe", last_name: "Becquey")
-christophe.photo.attach(io: file, filename: 'avatar', content_type: 'image')
-christophe.save
-
-file = URI.open('https://avatars.githubusercontent.com/u/80858493?v=4')
-dany = User.new(email: "dany@lewagon.fr", password: "444444", first_name: "Dany", last_name: "Renard")
-dany.photo.attach(io: file, filename: 'avatar', content_type: 'image')
-dany.save
-
-file = URI.open('https://avatars.githubusercontent.com/u/86717984?v=4')
-estelle = User.new(email: "estelle@lewagon.fr", password: "555555", first_name: "Estelle", last_name: "Dourthe")
-estelle.photo.attach(io: file, filename: 'avatar', content_type: 'image')
-estelle.save
-
-file = URI.open('https://avatars.githubusercontent.com/u/86261099?v=4')
-violette = User.new(email: "violette@lewagon.fr", password: "666666", first_name: "Violette", last_name: "Ruccio")
-violette.photo.attach(io: file, filename: 'avatar', content_type: 'image')
-violette.save
-
-file = URI.open('https://avatars.githubusercontent.com/u/86805683?v=4')
-maxence = User.new(email: "maxence@lewagon.fr", password: "777777", first_name: "Maxence", last_name: "Lerouge")
-maxence.photo.attach(io: file, filename: 'avatar', content_type: 'image')
-maxence.save
+puts "Creating #{loop_n} fake tasks, please wait..."
+loop_n.to_i.times do
+  task = Task.new(
+    title:    "#{Faker::Verb.simple_present} #{Faker::Hobby.activity}",
+    details:  Faker::Hacker.say_something_smart
+  )
+  task.save!
+end
 
 puts "Congrats, #{Task.count} tasks was created!"
 puts "=============================="
