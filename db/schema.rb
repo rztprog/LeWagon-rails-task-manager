8535143968c94d38716bc5b05d376715331ede83
Rzt Prog <rztprog@outlook.fr>
2022-04-20 15:50:04 +0200

    Change completed value to default false

--- db/schema.rb
+++ db/schema.rb
@@ -12,18 +12,10 @@
 
 ActiveRecord::Schema.define(version: 2021_08_10_094644) do
 
-  create_table "restaurants", force: :cascade do |t|
-    t.string "title"
-    t.text "details"
-    t.boolean "completed"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
   create_table "tasks", force: :cascade do |t|
     t.string "title"
     t.text "details"
-    t.boolean "completed"
+    t.boolean "completed", default: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
